ae2afc6a80a92fb7c5141a1be4ae1bb41119c1cb
chenwenjiang <chenwenjiang@meituan.com>
2021-01-11 15:47:59 +0800

    添加No.700 二叉搜索树中的搜索

---
+++ src/main/java/No700.java
@@ -0,0 +1,12 @@
+/**
+ * No.700 二叉搜索树中的搜索
+ * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ */
+public class No700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
+    }
+}
